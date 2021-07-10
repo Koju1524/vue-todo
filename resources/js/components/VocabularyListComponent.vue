e6fa2aea02631b7750efd1f9245d94300ff93cea
Koju Sakaguchi <fuetf@icloud.com>
2021-07-10 16:30:14 +0900

    create VocabularyListComponent

---
+++ resources/js/components/VocabularyListComponent.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+        <table class="table table-hover">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Word</th>
+                <th scope="col">Sentence</th>
+                <th scope="col">Show</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Title1</td>
+                <td>Content1</td>
+                <td>
+                    <button class="btn btn-primary">Show</button>
+                </td>
+                <td>
+                    <button class="btn btn-success">Edit</button>
+                </td>
+                <td>
+                    <button class="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Title2</td>
+                <td>Content2</td>
+                <td>
+                    <button class="btn btn-primary">Show</button>
+                </td>
+                <td>
+                    <button class="btn btn-success">Edit</button>
+                </td>
+                <td>
+                    <button class="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Title3</td>
+                <td>Content3</td>
+                <td>
+                    <button class="btn btn-primary">Show</button>
+                </td>
+                <td>
+                    <button class="btn btn-success">Edit</button>
+                </td>
+                <td>
+                    <button class="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
